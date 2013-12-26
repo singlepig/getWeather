a0046504e437e104566edf2a571537a251dac9e9
SinglePig <gys.nanjing@gmail.com>
2013-12-26 20:29:20 +0800

    把en文的地理位置信息也扣出来，一起显示出来。

--- getIpAddr.py
+++ getIpAddr.py
@@ -10,22 +10,31 @@ def getIpAddr():
     url = 'http://www.ip.cn'
     html = urllib2.urlopen(url).read()
     soup = BeautifulSoup(html)
-    # 分析http://www.ip.cn的源码，发现ip被<code>唯一包含，
-    # 所以通过find('code')进行解析，如果不唯一，可以找出所有，然后筛选
-    ip = soup.find('code').next #next用于去掉标签<code>
-    # 通过<p>标签找到位置信息
-    find_p = soup.find('p')
-    # 将beautifulsoup对象转换成字符串
-    str_find_p = str(find_p)
+    # 通过<div class="well">标签找到位置信息
+    find_div = soup.find('div',{'class':'well'})
+    ip = find_div.code.text
     # 定位地理位置信息
-    position = str_find_p.find(';')
-    # uft-8里中文占3字节，str()之后的结果一个字节是一个下标
-    # <p>当前 IP：<code>218.94.124.47</code>&nbsp;来自：江苏省南京市 电信</p>
-    # 来自： 各占3个下表，从江字开始，所以是position + 10
-    addr = str_find_p[position + 10:-4]
-    print ip
+    # 1.获得中文地理位置
+    no = find_div.contents[0].text.find(';')
+    addr = find_div.contents[0].text[no + 1:]
+    
+    # 2.获取en文地理位置
+    # <p>GeoIP: Nanjing, Jiangsu, China</p>
+    addrEn = find_div.contents[1].text
+    # 得到拼音字符串，然后进行分割,得到addr列表
+    addrEn = addrEn.split(':')
+    # addr取原列表中的最后一个元素
+    addrEn = addrEn[-1]
+    # 将包含位置信息的字符串用','再次分割，得到城市、省份
+    addrEn = addrEn.split(',')
+    # 去除字符串两边多余的空格
+    print '外网IP：', ip
     print addr
-    return [ip, addr]
+    for n in range(len(addrEn)):
+        addrEn[n] = addrEn[n].strip()
+        print addrEn[n], # 逗号是为了print后不换行
+    print
+    #print [ip, addr, addrEn]
 
 if __name__ == '__main__':
     getIpAddr()
